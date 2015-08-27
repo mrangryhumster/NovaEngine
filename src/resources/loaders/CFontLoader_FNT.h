4f93ee8a840af77cca365d48155cd756ed8d5c53
Gosha <mr.angryhumster@gmail.com>
2015-08-27 15:49:37 +0300

    FUCK!

---
+++ src/resources/loaders/CFontLoader_FNT.h
@@ -0,0 +1,45 @@
+#ifndef CFONTLOADER_FNT_H
+#define CFONTLOADER_FNT_H
+
+#include "CompileConfig.h"
+#ifdef NE_INCLUDE_FONT_LOADER_FNT
+
+#include "IFontLoader.h"
+#include "IResourceManager.h"
+#include "IFileSystem.h"
+#include "IFile.h"
+#include "NLogger.h"
+
+#include <cstring>
+#include <vector>
+#include <map>
+
+
+namespace novaengine
+{
+
+class CFontLoader_FNT : public IFontLoader
+{
+public:
+    CFontLoader_FNT(io::IFileSystem* fs,IResourceManager* rm);
+    virtual ~CFontLoader_FNT();
+
+    bool isSupported(const char* file_extension);
+    bool isSupported(io::IFile* file);
+
+    gui::IFont* LoadFont(const char* path);
+    gui::IFont* LoadFont(io::IFile* file);
+
+    gui::IFont* LoadFNT(io::IFile* file);
+
+protected:
+private:
+    void release_tmp_data(renderer::ITexture** TexturePages,u32 TexturePages_count,gui::SGlyph* Glyphs,u32 Glyphs_count);
+
+    io::IFileSystem* FileSystem;
+    IResourceManager* ResourceManager;
+};
+
+}
+#endif // NE_INCLUDE_FONT_LOADER_FNT
+#endif // CFONTLOADER_FNT_H
